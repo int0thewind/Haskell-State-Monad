2d9120173719d38b512bfdc5c075efd5f723cca2
Hanzhi Yin <yinhanzhi@gmail.com>
2020-08-06 18:24:00 -0500

    state applied

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,34 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+newtype State s a = StateConstructor { runState :: s -> (a, s) }
+
+instance Functor (State s) where
+    fmap f state = StateConstructor (\v -> let (x, v') = runState state v
+                                           in  (f x, v'))
+
+instance Applicative (State s) where
+    pure x = StateConstructor (\s -> (x, s))
+    -- Both the appling function and state are State
+    f <*> state = StateConstructor (\v -> let (a1, s1) = runState state v
+                                              (a2, s2) = runState f s1
+                                          in  (a2 a1, s2))
+
+instance Monad (State s) where
+    return = pure
+    state >>= f = StateConstructor (\v -> let (a, s') = runState state v
+                                          in  runState (f a) s')
+
+get :: p -> State a a
+get x = StateConstructor (\x -> (x, x))
+
+put :: s -> State s ()
+put x = StateConstructor (\s -> ((), x))
+
+inc :: Num a => a -> a
+inc = (+ 1)
+
+double :: Num a => a -> a
+double = (* 2)
+
+incState :: Num a => State a a
+incState = StateConstructor (\s -> (double s, inc s))
